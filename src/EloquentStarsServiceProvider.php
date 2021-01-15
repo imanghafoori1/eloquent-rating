219f94c5cff08279dfa09b528ca212bae1b797b0
imanghafoori <imanghafoori1@gmail.com>
2021-01-15 12:20:06 +0330

    setup

---
+++ src/EloquentStarsServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Imanghafoori\Stars;
+
+use Illuminate\Support\ServiceProvider;
+
+class EloquentStarsServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
