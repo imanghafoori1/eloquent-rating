3862ba44fca01e293effe56697e2be7ffc2ecd33
Iman Ghafoori <imanghafoori1@gmail.com>
2021-02-02 18:23:37 +0330

    added config file

--- src/EloquentStarsServiceProvider.php
+++ src/EloquentStarsServiceProvider.php
@@ -13,6 +13,13 @@ class EloquentStarsServiceProvider extends ServiceProvider
 
     public function boot()
     {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/stars.php' => config_path('star.php'),
+            ]);
+        }
+
+        $this->mergeConfigFrom(__DIR__. '/../config/stars.php', 'star');
         $this->loadMigrationsFrom(__DIR__. '/../migrations');
     }
 }
