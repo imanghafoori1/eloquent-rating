ff91f147e7668fb51b33adddd5ae281c84cf5936
Iman <imanghafoori1@gmail.com>
2021-02-05 19:36:52 +0330

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -12,20 +12,8 @@ if (! function_exists('getAvgRating')) {
     }
 }
 
-if (! function_exists('getRatingArray')) {
-    function getRatingArray($starCount, $total) {
-        return Imanghafoori\Stars\Star::getAvgRating($starCount, $total);
-    }
-}
-
 if (! function_exists('getRatings')) {
     function getRatings($starable, $starType = '_') {
         return Imanghafoori\Stars\Star::getRatings($starable, $starType = '_');
-    }
-}
-
-if (! function_exists('star')) {
-    function star($value, $userId, $starable, $starType = '_') {
-        Imanghafoori\Stars\Star::star($value, $userId, $starable, $starType = '_');
     }
 }
